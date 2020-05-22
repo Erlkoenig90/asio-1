ardentblue/asio

Add execution::context property.

asio/include/asio/execution/context.hpp
@@ -1,0 +1,143 @@
+//
+// execution/context.hpp
+// ~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef ASIO_EXECUTION_CONTEXT2_HPP
+#define ASIO_EXECUTION_CONTEXT2_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "asio/detail/config.hpp"
+#include "asio/detail/type_traits.hpp"
+#include "asio/execution/executor.hpp"
+#include "asio/is_applicable_property.hpp"
+#include "asio/traits/query_static_constexpr_member.hpp"
+#include "asio/traits/static_query.hpp"
+
+#include "asio/detail/push_options.hpp"
+
+namespace asio {
+namespace execution {
+namespace detail {
+
+template <int I = 0>
+struct context_t
+{
+#if defined(ASIO_HAS_VARIABLE_TEMPLATES)
+  template <typename T>
+  ASIO_STATIC_CONSTEXPR(bool,
+    is_applicable_property_v = is_executor<T>::value);
+#endif // defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+  ASIO_STATIC_CONSTEXPR(bool, is_requirable = false);
+  ASIO_STATIC_CONSTEXPR(bool, is_preferable = false);
+
+  ASIO_CONSTEXPR context_t()
+  {
+  }
+
+#if defined(ASIO_HAS_DECLTYPE) \
+  && defined(ASIO_HAS_NOEXCEPT) \
+  && defined(ASIO_HAS_VARIABLE_TEMPLATES)
+  template <typename T>
+  static ASIO_CONSTEXPR
+  typename traits::query_static_constexpr_member<T, context_t>::result_type
+  static_query()
+    ASIO_NOEXCEPT_IF((
+      traits::query_static_constexpr_member<T, context_t>::is_noexcept))
+  {
+    return traits::query_static_constexpr_member<T, context_t>::value();
+  }
+
+  template <typename E, typename T = decltype(context_t::static_query<E>())>
+  static ASIO_CONSTEXPR const T static_query_v
+    = context_t::static_query<E>();
+#endif // defined(ASIO_HAS_DECLTYPE)
+       //   && defined(ASIO_HAS_NOEXCEPT)
+       //   && defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+#if !defined(ASIO_HAS_CONSTEXPR)
+  static const context_t instance;
+#endif // !defined(ASIO_HAS_CONSTEXPR)
+};
+
+#if defined(ASIO_HAS_DECLTYPE) \
+  && defined(ASIO_HAS_NOEXCEPT) \
+  && defined(ASIO_HAS_VARIABLE_TEMPLATES)
+template <int I> template <typename E, typename T>
+const T context_t<I>::static_query_v;
+#endif // defined(ASIO_HAS_DECLTYPE)
+       //   && defined(ASIO_HAS_NOEXCEPT)
+       //   && defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+#if !defined(ASIO_HAS_CONSTEXPR)
+template <int I>
+const context_t<I> context_t<I>::instance;
+#endif
+
+} // namespace detail
+
+typedef detail::context_t<> context_t;
+
+#if defined(ASIO_HAS_CONSTEXPR) || defined(GENERATING_DOCUMENTATION)
+constexpr context_t context;
+#else // defined(ASIO_HAS_CONSTEXPR) || defined(GENERATING_DOCUMENTATION)
+namespace { static const context_t& context = context_t::instance; }
+#endif
+
+} // namespace execution
+
+#if !defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+template <typename T>
+struct is_applicable_property<T, execution::context_t>
+  : execution::is_executor<T>
+{
+};
+
+#endif // !defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+namespace traits {
+
+#if !defined(ASIO_HAS_DECLTYPE) \
+  || !defined(ASIO_HAS_NOEXCEPT) \
+  || !defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+template <typename T>
+struct static_query<T, execution::context_t,
+  typename enable_if<
+    traits::query_static_constexpr_member<T,
+      execution::context_t>::is_valid
+  >::type>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = true);
+
+  typedef typename traits::query_static_constexpr_member<T,
+    execution::context_t>::result_type result_type;
+
+  static ASIO_CONSTEXPR result_type value()
+  {
+    return traits::query_static_constexpr_member<T,
+      execution::context_t>::value();
+  }
+};
+
+#endif // !defined(ASIO_HAS_DECLTYPE)
+       //   || !defined(ASIO_HAS_NOEXCEPT)
+       //   || !defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+} // namespace traits
+} // namespace asio
+
+#include "asio/detail/pop_options.hpp"
+
+#endif // ASIO_EXECUTION_CONTEXT2_HPP

Erlkoenig90/asio-1

asio/include/asio/execution/context.hpp
@@ -1,0 +1,137 @@
+//
+// execution/context.hpp
+// ~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef ASIO_EXECUTION_CONTEXT2_HPP
+#define ASIO_EXECUTION_CONTEXT2_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "asio/detail/config.hpp"
+#include "asio/detail/type_traits.hpp"
+#include "asio/execution/executor.hpp"
+#include "asio/is_applicable_property.hpp"
+#include "asio/traits/query_static_constexpr_member.hpp"
+#include "asio/traits/static_query.hpp"
+
+#include "asio/detail/push_options.hpp"
+
+namespace asio {
+namespace execution {
+namespace detail {
+
+template <int I = 0>
+struct context_t
+{
+#if defined(ASIO_HAS_VARIABLE_TEMPLATES)
+  template <typename T>
+  ASIO_STATIC_CONSTEXPR(bool,
+    is_applicable_property_v = is_executor<T>::value);
+#endif // defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+  ASIO_STATIC_CONSTEXPR(bool, is_requirable = false);
+  ASIO_STATIC_CONSTEXPR(bool, is_preferable = false);
+
+  ASIO_CONSTEXPR context_t()
+  {
+  }
+
+#if defined(ASIO_HAS_DEDUCED_STATIC_QUERY_TRAIT) \
+  && defined(ASIO_HAS_SFINAE_VARIABLE_TEMPLATES)
+  template <typename T>
+  static ASIO_CONSTEXPR
+  typename traits::query_static_constexpr_member<T, context_t>::result_type
+  static_query()
+    ASIO_NOEXCEPT_IF((
+      traits::query_static_constexpr_member<T, context_t>::is_noexcept))
+  {
+    return traits::query_static_constexpr_member<T, context_t>::value();
+  }
+
+  template <typename E, typename T = decltype(context_t::static_query<E>())>
+  static ASIO_CONSTEXPR const T static_query_v
+    = context_t::static_query<E>();
+#endif // defined(ASIO_HAS_DEDUCED_STATIC_QUERY_TRAIT)
+       //   && defined(ASIO_HAS_SFINAE_VARIABLE_TEMPLATES)
+
+#if !defined(ASIO_HAS_CONSTEXPR)
+  static const context_t instance;
+#endif // !defined(ASIO_HAS_CONSTEXPR)
+};
+
+#if defined(ASIO_HAS_DEDUCED_STATIC_QUERY_TRAIT) \
+  && defined(ASIO_HAS_SFINAE_VARIABLE_TEMPLATES)
+template <int I> template <typename E, typename T>
+const T context_t<I>::static_query_v;
+#endif // defined(ASIO_HAS_DEDUCED_STATIC_QUERY_TRAIT)
+       //   && defined(ASIO_HAS_SFINAE_VARIABLE_TEMPLATES)
+
+#if !defined(ASIO_HAS_CONSTEXPR)
+template <int I>
+const context_t<I> context_t<I>::instance;
+#endif
+
+} // namespace detail
+
+typedef detail::context_t<> context_t;
+
+#if defined(ASIO_HAS_CONSTEXPR) || defined(GENERATING_DOCUMENTATION)
+constexpr context_t context;
+#else // defined(ASIO_HAS_CONSTEXPR) || defined(GENERATING_DOCUMENTATION)
+namespace { static const context_t& context = context_t::instance; }
+#endif
+
+} // namespace execution
+
+#if !defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+template <typename T>
+struct is_applicable_property<T, execution::context_t>
+  : execution::is_executor<T>
+{
+};
+
+#endif // !defined(ASIO_HAS_VARIABLE_TEMPLATES)
+
+namespace traits {
+
+#if !defined(ASIO_HAS_DEDUCED_STATIC_QUERY_TRAIT) \
+  || !defined(ASIO_HAS_SFINAE_VARIABLE_TEMPLATES)
+
+template <typename T>
+struct static_query<T, execution::context_t,
+  typename enable_if<
+    traits::query_static_constexpr_member<T,
+      execution::context_t>::is_valid
+  >::type>
+{
+  ASIO_STATIC_CONSTEXPR(bool, is_valid = true);
+  ASIO_STATIC_CONSTEXPR(bool, is_noexcept = true);
+
+  typedef typename traits::query_static_constexpr_member<T,
+    execution::context_t>::result_type result_type;
+
+  static ASIO_CONSTEXPR result_type value()
+  {
+    return traits::query_static_constexpr_member<T,
+      execution::context_t>::value();
+  }
+};
+
+#endif // !defined(ASIO_HAS_DEDUCED_STATIC_QUERY_TRAIT)
+       //   || !defined(ASIO_HAS_SFINAE_VARIABLE_TEMPLATES)
+
+} // namespace traits
+} // namespace asio
+
+#include "asio/detail/pop_options.hpp"
+
+#endif // ASIO_EXECUTION_CONTEXT2_HPP